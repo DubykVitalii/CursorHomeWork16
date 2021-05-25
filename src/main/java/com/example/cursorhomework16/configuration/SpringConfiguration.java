e563536b826aa4edc58600f85b4495ff33b9fc9e
DubykVitalii <dybukvitalik@gmail.com>
2021-05-25 10:28:17 +0300

    Fix spring configuration

--- src/main/java/com/example/cursorhomework16/configuration/SpringConfiguration.java
+++ src/main/java/com/example/cursorhomework16/configuration/SpringConfiguration.java
@@ -1,10 +1,10 @@
 package com.example.cursorhomework16.configuration;
 
 import com.example.cursorhomework16.components.Head;
-import org.springframework.beans.factory.annotation.Configurable;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 
-@Configurable
+@Configuration
 public class SpringConfiguration {
 
     @Bean
